ee92971e3631e123c7f8d659868b809ce7e5e9e7
Aldescu Marian <marian.aldescu11@gmail.com>
2019-04-07 12:04:38 +0200

    finished iteration 1

--- client.c
+++ client.c
@@ -3,42 +3,59 @@
 #include <stdlib.h> 
 #include <string.h> 
 #include <sys/socket.h> 
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include "helpers.h"
 
 #define MAX 80 
-//#define PORT 44947 
+
 #define IP "127.0.0.1"
 #define PORT 8080
 #define SA struct sockaddr 
+ 
 
-void func(int sockfd) 
-{ 
-    char buff[MAX]; 
-    int n; 
-    while (1) { 
-        bzero(buff, sizeof(buff)); 
-        printf("Send the string: "); 
-        n = 0; 
-        while ((buff[n++] = getchar()) != '\n');
- 
-        send(sockfd, buff, sizeof(buff), 0); 
- 
-        bzero(buff, sizeof(buff)); 
- 
-        recv(sockfd, buff, sizeof(buff), 0); 
-        printf("From Server : %s\n", buff); 
+/* function
+ * Reads a message from keyboard into the buffer and sends to
+ * the channel defined by sockfd
+*/
+void type_and_send(char* buffer, int dimens, int sockfd) {
+	int pos = 0;
+	int return_val;
 
-        if ((strncmp(buff, "exit", 4)) == 0) { 
-            printf("Client Exit...\n"); 
-            break; 
-        } 
-    } 
-} 
-  
-int main() 
+	printf("You> ");
+	while ((buffer[pos++] = getchar()) != '\n');
+	return_val = send(sockfd, buffer, dimens, 0); 
+	CHECK(return_val < 0, "Error receiving message from server");
+}
+
+
+int CHECK_fin_message(char* message) {
+	if (strncmp(message, "fin", 3) == 0)
+	{
+		printf("Client has stopped\n");
+		return 1;
+	}
+	return 0;
+}
+
+int main(int argc, char* argv[]) 
 {
 
-	int sockfd, connfd; 
-	struct sockaddr_in servaddr, cli; 
+	char buff_send[MAX];
+	char buff_recv[MAX];
+
+	int sockfd; 
+	int return_val;
+	int stop_chat = 0;
+
+	struct sockaddr_in servaddr; 
+
+	if (argc < 3)
+	{
+		printf("Args: parameter for server's ip&port is missing\n");	
+		exit(0);
+	}
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -51,16 +68,38 @@ int main()
 	bzero(&servaddr, sizeof(servaddr)); 
 
 	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-	//servaddr.sin_addr.s_addr = inet_addr(IP); 
+	// argv[1] is the server's ip
+	servaddr.sin_addr.s_addr = inet_addr(argv[1]); 
+	// argv[2] is the server's port on which it is listening
+	servaddr.sin_port = htons(atoi(argv[2])); 
 
-	servaddr.sin_port = htons(PORT); 
+	return_val = connect(sockfd, (SA*)&servaddr, sizeof(servaddr));
+	CHECK(return_val != 0, "Cannot connect to the server\n");
 
-	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
-		printf("Cannot connect to the server\n");
+	// Client is waiting for hello message from server
+	bzero(buff_recv, MAX);
+	return_val = recv(sockfd, buff_recv, MAX, 0); 
+	CHECK(return_val < 0, "Error receiving the message from server");
+
+    printf("From server: %s\n", buff_recv);
+
+    while (!stop_chat) {
+		// Client is waiting for a message from server
+		bzero(buff_recv, MAX);
+		return_val = recv(sockfd, buff_recv, MAX, 0); 
+		CHECK(return_val < 0, "Error receiving the message from server");
+
+	    printf("From server> %s\n", buff_recv);
+
+	    // If "fin" message is received, stop the chat
+		stop_chat = CHECK_fin_message(buff_recv);
+	    if (!stop_chat) {
+
+			// Type a message to send to the server
+			type_and_send(buff_send, MAX, sockfd);
+			stop_chat = CHECK_fin_message(buff_send);
+		}
 	}
-
-	func(sockfd); 
 
 	close(sockfd); 
 } 
---
+++ helpers.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* useful macro for handling error codes */
+#define CHECK(assertion, call_description)	\
+	do {									\
+		if (assertion) {					\
+			fprintf(stderr, "(%s, %d): ",	\
+					__FILE__, __LINE__);	\
+			perror(call_description);		\
+			exit(EXIT_FAILURE);				\
+		}									\
+	} while (0)
+
